84f36626adb8d30b1a8e2a09424f332d214d2141
wave <wind@wave>
2020-08-08 12:11:17 +0300

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+const CardanoV4 = require('@emurgo/cardano-serialization-lib-nodejs')
+const nacl = require('./nacl-fast')
+
+const base58 = 'DdzFFzCqrht34sAkDJ1LQK4Ee98AkizbWKs5V3zmiCweGaDZ4kjxtGBkF4VQWZX9VMcMmfrKrd18fotFicnj1NTVEz6ZxoTKktMgt4CU'
+
+console.log(
+  CardanoV4.ByronAddress.from_base58(base58).to_bytes().length,
+)
