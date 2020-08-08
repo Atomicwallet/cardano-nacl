0bde31286deb0547e4d7b3aa5e4b2420de440a3d
wave <wind@wave>
2020-08-08 12:12:13 +0300

    comment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const CardanoV4 = require('@emurgo/cardano-serialization-lib-nodejs')
-const nacl = require('./nacl-fast')
+const nacl = require('./nacl-fast') // comment me
 
 const base58 = 'DdzFFzCqrht34sAkDJ1LQK4Ee98AkizbWKs5V3zmiCweGaDZ4kjxtGBkF4VQWZX9VMcMmfrKrd18fotFicnj1NTVEz6ZxoTKktMgt4CU'
 
